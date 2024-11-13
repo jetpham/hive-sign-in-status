8d7429337db40c3cbabda59b54a5fd090f726b74
Jet Pham <55770902+jetpham@users.noreply.github.com>
2024-11-12 23:48:51 -0800

    fix: Google Fonts now a thing ig

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { Geist, Geist_Mono } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: "Hive Sign In Status",
   description: "A dashboard to track Hive sign ins",
 };
+
+const geistSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+});
 
 export default function RootLayout({
   children,
