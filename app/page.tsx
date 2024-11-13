279b8efafb18d923c5638f792f3f5bd696d485e2
Jet Pham <55770902+jetpham@users.noreply.github.com>
2024-11-12 20:43:11 -0800

    feat: add Links and title

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,19 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 import { PrismaClient } from "@prisma/client";
+import Link from "next/link";
+import { Badge, badgeVariants } from "@/components/ui/badge"
+
 
 const prisma = new PrismaClient();
 
@@ -33,34 +45,45 @@ export default async function Home() {
   });
 
   return (
-    <Table>
-      <TableCaption>A list of recent data updates.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>ID</TableHead>
-          <TableHead>Name</TableHead>
-          <TableHead>Duration</TableHead>
-          <TableHead>Areas</TableHead>
-          <TableHead>Major</TableHead>
-          <TableHead>Project</TableHead>
-          <TableHead>Opinion</TableHead>
-          <TableHead>Created At</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {updatedData.map((data) => (
-          <TableRow key={data.id}>
-            <TableCell>{data.id}</TableCell>
-            <TableCell>{data.name}</TableCell>
-            <TableCell>{data.duration}</TableCell>
-            <TableCell>{data.areas}</TableCell>
-            <TableCell>{data.major}</TableCell>
-            <TableCell>{data.project}</TableCell>
-            <TableCell>{data.opinion}</TableCell>
-            <TableCell>{new Date(data.createdAt).toLocaleString()}</TableCell>
+    <>
+      <Card>
+        <CardHeader style={{ display: 'flex', alignItems: 'center' }}>
+          <CardTitle style={{ paddingBottom: '10px' }}>Hive Sign Ins</CardTitle>
+          <div style={{ display: 'flex', gap: '8px' }}>
+            <Link className={badgeVariants({ variant: "outline" })} href={"https://github.com/jetpham/hive-sign-in-status"}>Github Repo</Link>
+            <Link className={badgeVariants({ variant: "outline" })} href={"https://jetpham.com/"}>Jet's Blog</Link>
+            <Link className={badgeVariants({ variant: "outline" })} href={"https://docs.google.com/forms/d/e/1FAIpQLSe9T3XuEwjwAPiANzvVZwvLueLBsqZZP569yjOm8rQ5OFZsYQ/viewform"}>Hive Sign In Form</Link>
+          </div>
+        </CardHeader>
+      </Card>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Duration</TableHead>
+            <TableHead>Areas</TableHead>
+            <TableHead>Major</TableHead>
+            <TableHead>Project</TableHead>
+            <TableHead>Opinion</TableHead>
+            <TableHead>Created At</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {updatedData.map((data) => (
+            <TableRow key={data.id}>
+              <TableCell>{data.id}</TableCell>
+              <TableCell>{data.name}</TableCell>
+              <TableCell>{data.duration}</TableCell>
+              <TableCell>{data.areas}</TableCell>
+              <TableCell>{data.major}</TableCell>
+              <TableCell>{data.project}</TableCell>
+              <TableCell>{data.opinion}</TableCell>
+              <TableCell>{new Date(data.createdAt).toLocaleString()}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 }
