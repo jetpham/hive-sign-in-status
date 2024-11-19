7c67146c42ba26fdff35795c38289483e1081126
Jet Pham <55770902+jetpham@users.noreply.github.com>
2024-11-18 23:23:27 -0800

    fix: move fetch to entries and give suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client";
 import Entries from "@/components/entries";
-import { unstable_cache } from "next/cache";
-import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { Suspense } from "react";
 
 export type UpdatedDataType = {
   id: number;
@@ -14,31 +12,10 @@ export type UpdatedDataType = {
   timestamp: Date;
 };
 
-const prisma = new PrismaClient();
-
-const getCachedEntries = unstable_cache(async () => {
-  "use cache";
-  cacheTag("entries");
-  console.log("Fetching updated data");
-  return await prisma.updatedData.findMany({
-    where: {
-      timestamp: {
-        gte: new Date(new Date().setDate(new Date().getDate() - 1)),
-      },
-    },
-    orderBy: {
-      timestamp: "desc",
-    },
-  });
-});
-
 export default async function Home() {
-  const updatedData: UpdatedDataType[] = await getCachedEntries();
-
-  //TODO: add a qr code for the hive sign in form
   return (
-    <>
-      <Entries updatedData={updatedData} />
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Entries />
+    </Suspense>
   );
 }
--- components/entries.tsx
+++ components/entries.tsx
@@ -1,12 +1,29 @@
 import Entry from "./entry";
 import { UpdatedDataType } from "@/app/page";
 import { ScrollArea } from "./ui/scroll-area";
+import { PrismaClient } from "@prisma/client";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+const prisma = new PrismaClient();
 
-export default function Entries({
-  updatedData,
-}: {
-  updatedData: UpdatedDataType[];
-}) {
+export async function getCachedEntries() {
+  "use cache";
+  cacheTag("entries");
+  console.log("Fetching updated data");
+  return await prisma.updatedData.findMany({
+    where: {
+      timestamp: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 1)),
+      },
+    },
+    orderBy: {
+      timestamp: "desc",
+    },
+  });
+}
+
+export default async function Entries() {
+  const updatedData: UpdatedDataType[] = await getCachedEntries();
+
   return (
     <ScrollArea className="h-full w-full rounded-md border">
       {updatedData.map((data) => (
